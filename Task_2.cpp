e4f9d968729634070e9cd599258018ca8e5bc15f
Gavriel-Sembiring-366 <gavrielsembiring366@gmail.com>
2023-05-09 21:55:32 +0700

    first commit

---
+++ Task_2.cpp
@@ -0,0 +1,49 @@
+// maaf ga sempat buat comment bang :)
+// semoga bisa dimengerti bang
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int freq, bil;
+
+void frek(vector<int> angka)
+{
+    for (int i = 0; i < angka.size(); i++)
+    {
+        if (angka[i] == bil)
+        {
+            freq++;
+        }
+    }
+
+    if (freq > 0)
+    {
+        cout << "Frekuensi dari angka " << bil << " di dalam array adalah " << freq << endl;
+    }
+    else
+    {
+        cout << "Angka tidak ditemukan\n";
+    }
+}
+
+int main(void)
+{
+    int temp, size;
+    vector<int> angka;
+    cout << "Masukkan ukuran vector: ";
+    cin >> size;
+
+    for (int i = 1; i <= size; i++)
+    {
+        cout << "Masukkan angka ke-" << i << ": ";
+        cin >> temp;
+        angka.push_back(temp);
+    }
+
+    cout << "Masukkan angka yang mau dicari frekuensinya: ";
+    cin >> bil;
+
+    frek(angka);
+}
